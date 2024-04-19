b248083a50c977d39f7045165d970409bbef88d4
JeromeGarcia8 <jerome.garcia1254@gmail.com>
2024-04-19 15:36:59 -0700

    Website Deploy

---
+++ js/index.js
@@ -0,0 +1,76 @@
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.hero-text', {
+    xPercent: -500,
+    duration: 2.5,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: ".hero-container",
+        markers: true,
+    }
+});
+
+
+gsap.from('.text-align-center', {
+    xPercent: -300,
+    duration: 2.5,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: ".flexcontainer",
+        start: "top 80%",
+    }
+
+});
+
+gsap.from('.col-4, .container', {
+    xPercent: -500,
+    duration: 2.5,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: ".text-align-right",
+        start: "top 50%",
+        markers: true,
+    }
+});
+
+gsap.from('.col-6', {
+    xPercent: -500,
+    duration: 2.5,
+    ease: "power4.out",
+    scrollTrigger: {
+        trigger: ".text-align-right",
+        start: "top 50%",
+        markers: true,
+    }
+});
+
+
+
+
+gsap.from('.header', {
+    scale: 400,
+    duration: 1,
+    ease: "power4.out",
+    
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
